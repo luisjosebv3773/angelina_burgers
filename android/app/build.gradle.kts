7b0d95045576cf3a1a835ad0f756fbd26ff6222c
luis briceño <luisbriceno@Mac-mini-de-luis.local>
2026-04-01 21:18:13 -0400

    Rename project internally: angelina_burguers -> angelina_burgers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.angelina_burguers"
+    namespace = "com.example.angelina_burgers"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.angelina_burguers"
+        applicationId = "com.example.angelina_burgers"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/angelina_burgers/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/angelina_burgers/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.angelina_burguers
+package com.example.angelina_burgers
 
 import io.flutter.embedding.android.FlutterActivity
 
